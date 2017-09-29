27ef4fa2c32d2e1d480da908c9d211cea19530a1
Kwangsung <ohxxx222@umn.edu>
2017-09-29 17:21:13 +0000

    Remove not used code

--- wieracentral/WieraThriftIface.thrift
+++
@@ -1,6 +0,0 @@
-service WieraThriftIface {
-    string createInstances(1:string policy);
-    string startInstances(1:string key);
-    string stopInstances(1:string key);
-    string getInstances(1:string key);
-}
--- wieracentral/wieraThrift.thrift
+++
@@ -1,6 +0,0 @@
-service wieraThrift {
-    bool createInstances(1:string policy);
-    bool startInstances(1:string key);
-    bool stopInstances(1:string key);
-    bool getInstances(1:string key);
-}
